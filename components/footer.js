f96b9ca1a11fa605346c11f6d78217cfc552a4d4
Ryan Le <31494954+lryanle@users.noreply.github.com>
2023-01-21 20:30:46 -0600

    redo lol

--- components/footer.js
+++ components/footer.js
@@ -1,30 +1,202 @@
-import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
-
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
-      <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            {/* Statically Generated with Next.js. */}
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+    <footer className="pb-4">
+      <div className="max-w-6xl xl:max-w-6xl mx-auto divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
+        <ul className="Footer_nav__2rFid text-sm font-medium  sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
+          <li className="space-y-5 row-span-2">
+            <h2 className="text-sm tracking-wide text-gray-900 uppercase font-bold">
+              Company
+            </h2>
+            <ul className="space-y-4 text-md">
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Jobs
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Merch
+                </a>
+              </li>{" "}
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Brand
+                </a>
+              </li>{" "}
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Meetups
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="space-y-5 row-span-2">
+            <h2 className="text-sm tracking-wide text-gray-900 uppercase font-bold">
+              Newsroom
+            </h2>
+            <ul className="space-y-4">
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  News
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Press
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="space-y-5 row-span-2">
+            <h2 className="text-sm tracking-wide text-gray-900 uppercase font-bold">
+              Products
+            </h2>
+            <ul className="space-y-4">
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Hosting
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Domains
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Security
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  SSL
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="space-y-5">
+            <h2 className="text-sm tracking-wide text-gray-900 uppercase font-bold">
+              Connect
+            </h2>
+            <ul className="space-y-4">
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 text-2xl"
+                  href="/"
+                >
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-900 transition-colors duration-200 font-semibold text-2xl"
+                  href="/"
+                >
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <div className="flex flex-col-reverse justify-between pt-5 pb-4 border-t lg:flex-row bg-top border-black">
+          <ul className="flex flex-col space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
+            <li>
+              <a
+                href="/"
+                className="text-md text-black transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
+              >
+                Terms of Service
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="text-md text-black transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="text-md text-black transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
+              >
+                Ad Choices
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="text-md text-black transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
+              >
+                Cookie Policy
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="text-md text-black transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
+              >
+                Partners
+              </a>
+            </li>
+          </ul>
+          <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              href="/"
+              className="text-md text-black transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold tracking-tight"
             >
-              Read Documentation
+              © 2021 Company Inc.
             </a>
-            <a
-              href={`https://github.com/lryanle/stf-site/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div>
+          </ul>
         </div>
-      </Container>
+      </div>
     </footer>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,27 @@
-import Head from 'next/head'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { CMS_NAME } from '../lib/constants'
-import { indexQuery } from '../lib/queries'
-import { usePreviewSubscription } from '../lib/sanity'
-import { getClient, overlayDrafts } from '../lib/sanity.server'
+import Head from "next/head";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+import { NextSeo } from "next-seo";
 
-export default function Index({ allPosts: initialAllPosts, preview }) {
-  const { data: allPosts } = usePreviewSubscription(indexQuery, {
-    initialData: initialAllPosts,
-    enabled: preview,
-  })
-  const [heroPost, ...morePosts] = allPosts || []
+export default function Home() {
   return (
-    <>
-      <Layout preview={preview}>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
-}
-
-export async function getStaticProps({ preview = false }) {
-  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery))
-  return {
-    props: { allPosts, preview },
-    // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
-    revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
-  }
+    <div className="text-black">
+      <NextSeo
+        title="Home: nine4"
+        description="Welcome to nine4 homepage."
+        canonical="https://nine4-2.vercel.app/"
+        openGraph={{
+          url: "https://nine4-2.vercel.app/",
+        }}
+      />
+      <Head>
+        <title>nine4</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
